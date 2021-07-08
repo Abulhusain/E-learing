1c9fd4e5683d0da68f8c8c6d4c0720572f9560b6
Abulhusain <abulhusainagaria@gmail.com>
2021-07-08 13:50:41 +0530

    changes done

---
+++ classroom/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Answer)
+# admin.site.register(Course)
+# admin.site.register(MyFile)
+admin.site.register(Lesson)
+admin.site.register(Question)
+admin.site.register(Quiz)
+admin.site.register(StudentAnswer)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(TakenCourse)
+# admin.site.register(TakenQuiz)
+# admin.site.register(Teacher)
+admin.site.register(User)
+# admin.site.register(UserLog)
+
+
+
+
+
+
+
+class MyModelAdmin(admin.ModelAdmin):
+    fields = ('title','code','description','image','status','subject')
+
+
+admin.site.register(Course, MyModelAdmin)
